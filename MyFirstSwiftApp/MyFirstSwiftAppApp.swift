cde60419c048a62856897e6451582a4ee68f0b03
aka <aka@startext.dev>
2023-12-21 08:30:58 +0700

    Initial Commit

---
+++ MyFirstSwiftApp/MyFirstSwiftAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyFirstSwiftAppApp.swift
+//  MyFirstSwiftApp
+//
+//  Created by Arseniy Karmanov on 21.12.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyFirstSwiftAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
